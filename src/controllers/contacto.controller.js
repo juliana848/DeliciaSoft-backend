231dc6f2dd25fdcd2913ac132b427dcefebec735
juliana848 <julianaaquinterom@gmail.com>
2025-09-06 20:03:39 -0500

    correciones

--- src/controllers/contacto.controller.js
+++ src/controllers/contacto.controller.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const axios = require('axios');
 
 // Configurar transportador de nodemailer
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
