8619ee49c7238149f189ef2c494b6a6d04c31170
juliana848 <julianaaquinterom@gmail.com>
2025-09-06 21:25:50 -0500

    correciones

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -9,8 +9,8 @@ router.get('/:id', clienteController.getCliente);
 router.post('/', clienteController.createCliente);
 router.put('/:id', clienteController.updateCliente);
 router.delete('/:id', clienteController.deleteCliente);
-router.get('/:id/ventas', clienteTieneVentas);
-router.patch('/:id/password', actualizarContrasenaCliente);
+router.get('/:id/ventas', clienteController.clienteTieneVentas);
+router.patch('/:id/password', clienteController.actualizarContrasenaCliente);
 
 // Ruta para activar/desactivar cliente
 router.patch('/:id/estado', clienteController.toggleEstadoCliente);
