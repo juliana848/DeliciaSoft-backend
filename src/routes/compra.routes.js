ca88df8d81354a11750fa56ee813e1f89dd42d9e
Danna <dannaferreira1054@gmail.com>
2025-09-29 13:29:19 -0500

    cambiando en compras

--- src/routes/compra.routes.js
+++ src/routes/compra.routes.js
@@ -5,8 +5,12 @@ const controller = require('../controllers/compra.controller');
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
+
+// RUTAS ESPECÍFICAS PRIMERO (antes de la ruta genérica)
+router.put('/:id/anular', controller.anular);
+router.put('/:id/activar', controller.activar);
+
+// RUTA GENÉRICA AL FINAL
 router.put('/:id', controller.update);
-router.patch('/:id', controller.anular);
 
 module.exports = router;
-
